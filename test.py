f3d06034475be3c22be53fd75f9f27e4a9b670dc
Christopher Barlow <132295304+CbarNC@users.noreply.github.com>
2023-10-20 14:38:01 -0400

    Update test.py
    Swapped the typing and clicking code around

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 options = webdriver.EdgeOptions()
 options.add_experimental_option("detach", True)  # Keeps the webpage from automatically closing
 driver = webdriver.Edge(options=options)
@@ -11,19 +10,21 @@ driver.get("https://cbarnc.github.io/Group3-repo-projects/")
 # maximizing window
 driver.maximize_window()
 
-# click a button
-# button = driver.find_element(By.CLASS_NAME, "my-button-class")
-# button.click()
-
 # typing in comment box
-driver.find_element(By.ID,"comments").send_keys("Love this Place!")
+driver.find_element(By.ID, "comments").send_keys("Love this Place!")
+time.sleep(5)
+
+# click a button
+button = driver.find_element(By., "btn.btn-primary")
+button.click()
+time.sleep(5)
 
 # returning title of page
 print("Page title is: ")
 print(driver.title)
 
 # time before window closes
-time.sleep(13)
+time.sleep(5)
 
 # Close Edge browser
 driver.quit()
