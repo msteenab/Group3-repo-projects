e93b9ce8f07ce7dba859ff794b806e2348f38b19
JMoukas <144281540+JMoukas@users.noreply.github.com>
2023-10-20 16:53:49 -0400

    changes to javascript and menu page
    removed hyperlinks to individual foods on menu page
    
    removed order from menu page
    
    change javascript code to clear red input from sign up page once user corrects invalid character length

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 function setFormMessage(formElement, type, message) {
     const messageElement = formElement.querySelector(".form__message");
-
     messageElement.textContent = message;
     messageElement.classList.remove("form__message--success", "form__message--error");
     messageElement.classList.add(`form__message--${type}`);
+}
 
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
 }
 
 function setInputError(inputElement, message) {
@@ -12,11 +15,9 @@ function setInputError(inputElement, message) {
     inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
 }
 
-
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#Login");
     const createAccountForm = document.querySelector("#createAccount");
-
 
     document.querySelector("#linkCreateAccount").addEventListener("click", e => {
         e.preventDefault();
@@ -32,7 +33,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     loginForm.addEventListener("submit", e => {
         e.preventDefault();
-
         setFormMessage(loginForm, "error", "Invalid username or password ");
     });
 
@@ -42,7 +42,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 setInputError(inputElement, "username must be at least 10 characters in length");
             }
         });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
     });
 });
-
-
