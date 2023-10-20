1778c754e6c98115890c56b68491a48703c96f38
msteenab <kbridges@my.waketech.edu>
2023-10-20 00:02:59 -0400

    wrote script and tested writing text in comment box, returning title page to server and clsoing broswer after 13 sec delay to allow text to be added

--- test.py
+++ test.py
@@ -1,16 +1,29 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
 
 options = webdriver.EdgeOptions()
 options.add_experimental_option("detach", True)  # Keeps the webpage from automatically closing
 driver = webdriver.Edge(options=options)
 driver.get("https://cbarnc.github.io/Group3-repo-projects/")
 
+# maximizing window
+driver.maximize_window()
+
 # click a button
-button = driver.find_element(By.CLASS_NAME, "my-button-class")
-button.click()
+# button = driver.find_element(By.CLASS_NAME, "my-button-class")
+# button.click()
 
-# locate elements
-element_by_id = driver.find_element(By.ID, "element_id")
+# typing in comment box
+driver.find_element(By.ID,"comments").send_keys("Love this Place!")
 
+# returning title of page
+print("Page title is: ")
+print(driver.title)
 
+# time before window closes
+time.sleep(13)
+
+# Close Edge browser
+driver.quit()
