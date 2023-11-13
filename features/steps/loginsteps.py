1e04792397e5c76a06d143a9cc6eef675287b447
Aliyah <aliyah.y.lewis@gmail.com>
2023-11-12 21:11:01 -0500

    login-steps update

--- features/steps/loginsteps.py
+++ features/steps/loginsteps.py
@@ -11,3 +11,17 @@ def loginPage(context):
 @when('they enter valid username and password')
 def userPass(context):
     username_input = context.driver.find_element_by_id('username')
+    password_input = context.driver.find_element_by_id('password')
+
+    username_input.send_keys('test_user123')
+    password_input.send_keys('password123')
+
+@when('click the "Login" button')
+def clickLogin(context):
+    login_button = context.driver.find_element_by_id('login_button')
+    login_button.click()
+
+@then('they should be redirected to the dashboard')
+def redirect(context):
+    current_url = context.driver.current_url
+    assert current_url == 'https://cbarnc.github.io/Group3-repo-projects/'
