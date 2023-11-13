d5824b5c12ef8ba9513442db32d3b3cb6918d9d1
Aliyah <aliyah.y.lewis@gmail.com>
2023-11-12 21:28:27 -0500

    Update loginsteps.py

--- features/steps/loginsteps.py
+++ features/steps/loginsteps.py
@@ -10,15 +10,15 @@ def loginPage(context):
 
 @when('they enter valid username and password')
 def userPass(context):
-    username_input = context.driver.find_element_by_id('username')
-    password_input = context.driver.find_element_by_id('password')
+    username_input = context.driver.find_elements(By.ID, 'username')
+    password_input = context.driver.find_elements(By.ID, 'password')
 
     username_input.send_keys('test_user123')
     password_input.send_keys('password123')
 
 @when('click the "Login" button')
 def clickLogin(context):
-    login_button = context.driver.find_element_by_id('login_button')
+    login_button = context.driver.find_elements(By.ID, 'login_button')
     login_button.click()
 
 @then('they should be redirected to the dashboard')
