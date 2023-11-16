7f16c6bc6981002a1ac2ddb24fa477c63d746c1c
Christopher Barlow <132295304+CbarNC@users.noreply.github.com>
2023-11-16 14:40:42 -0500

    Update menusteps.py

--- features/steps/menusteps.py
+++ features/steps/menusteps.py
@@ -1,3 +1,5 @@
+import time
+
 from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,20 +8,23 @@ from selenium.webdriver.common.by import By
 @given('the user is on the restaurant\'s homepage')
 def homePage(context):
     context.driver = webdriver.Chrome()
-
     context.driver.get("https://cbarnc.github.io/Group3-repo-projects/")
 
 
 @when('they click on the "Menu" section')
 def openMenu(context):
-    context.driver.get("https://cbarnc.github.io/Group3-repo-projects/menu.html")
+    menu_button = context.driver.find_element(By.LINK_TEXT, 'MENU')
+    menu_button.click()
+    time.sleep(2)
 
 
 @then('they should see a list of dishes and their prices')
 def menuList(context):
-    context.driver.find_elements(By.CLASS_NAME, 'menu')
+    menu_elements = context.driver.find_elements(By.CLASS_NAME, 'menu')
+    assert len(menu_elements) > 0, "No 'menu' elements found on the page"
+    time.sleep(2)
 
 
-@then(u'close browser')
+@then('close browser')
 def closeBrowser(context):
     context.driver.close()
