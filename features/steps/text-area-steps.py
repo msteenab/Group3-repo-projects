518238f648b7d9664cc3a064e961866913f7aa7b
msteenab <kbridges@my.waketech.edu>
2023-11-16 03:20:45 -0500

    finished the text.area test, it will show a screenshot before text is written and another screenshot after text is written

---
+++ features/steps/text-area-steps.py
@@ -0,0 +1,38 @@
+import time
+from Screenshot import *
+from behave import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from PIL import Image
+
+
+@given(u'The comments box is empty')
+def emptyComments(context):
+    context.driver = webdriver.Chrome()
+    context.driver.maximize_window()
+    context.driver.get('https://cbarnc.github.io/Group3-repo-projects/')
+    assert context.driver.find_element(By.ID, 'comments').text == ''
+    # takes a screenshot of the empty text area before user writes
+    ob = Screenshot.Screenshot()
+    img_url = ob.full_screenshot(context.driver, save_path=r'.', image_name='myimage2.png', is_load_at_runtime=True,
+                                 load_wait_time=0)
+    screenshot = Image.open(img_url)
+    screenshot.show()
+
+
+@when(u'The user selects the input box and types a message')
+def testMessage(context):
+    sendMessage = "This is a Test Message"
+    context.driver.find_element(By.ID, 'comments').send_keys(sendMessage)
+    time.sleep(3)
+    ob = Screenshot.Screenshot()
+    img_url2 = ob.full_screenshot(context.driver, save_path=r'.', image_name='myimage.png', is_load_at_runtime=True,
+                                  load_wait_time=0)
+    print(img_url2)
+
+
+# takes a screenshot of message user wrote
+@then(u'The comment box is filled')
+def messageStatus(context):
+    screenshot = Image.open("myimage.png")
+    screenshot.show()
--- features/text-area.feature
+++ features/text-area.feature
@@ -1,12 +1,7 @@
 Feature: Contact Us Box - Text Area
 
-  Scenario: User submits a message via the contact form
-    Given the user is on the Contact Us page
-    When the user fills in their name "John Doe"
-    And the user enters their email "johndoe@example.com"
-    And the user writes a message in the text area:
-      """
-      This is a test message.
-      """
-    And the user clicks the "Submit" button
-    Then the message should be sent successfully
+  Scenario: Test the text box area
+      Given The comments box is empty
+      When The user selects the input box and types a message
+      Then The comment box is filled
+
